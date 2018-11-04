8a2eeca49dedc09bfd54c55270df48604121b5d0
Mo Dongsong <modongsong@126.com>
2018-11-04 22:40:35 +0800

    Updated:

--- config/setup.sh
+++ config/setup.sh
@@ -1,2 +1,24 @@
 #!/usr/bin/env bash
 
+function setupCentosRepos(){
+    sudo mkdir /etc/repos.d.bak
+    sudo mv /etc/repos.d/*   /etc/repos.d.bak
+    sudo cp etc/repos.d/nexus.repo   /etc/repos.d
+}
+
+function setupDocker(){
+    sudo cp etc/docker/daemon.json  /etc/docker
+}
+
+function setupPki(){
+    sudo cp etc/pki/rpm-gpg/*     /etc/pki/rpm-gpg
+}
+
+
+function run(){
+    setupCentosRepos
+    setupDocker
+    setupPki
+}
+
+run
