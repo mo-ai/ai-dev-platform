0cdf83f0c421d2a52169d3483008be3b22c1fb1f
Mo Dongsong <modongsong@126.com>
2018-11-05 13:54:36 +0800

    Updated:

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+curDir=`pwd`
+baseDir=$(dirname $0)
+cd $baseDir
+home=`pwd`
+
+components_dir=${baseDir}/components
+component=$1
+components=`ls ${components_dir}`
+
+function checkComponent(){
+     if [ "$component" = "" ]; then
+         echo "Please input the component name: ${components}"
+         exit 1
+     fi
+
+     if ! ls ${components_dir}|grep ${component}; then
+        echo "Please input the correct component name: ${component} ; component options: ${components}"
+         exit 1
+     fi
+}
+
+function deployComponent(){
+     sh ${components_dir}/${component}/deploy.sh
+}
+
+function start(){
+    checkComponent
+    deployComponent
+}
+
+start
