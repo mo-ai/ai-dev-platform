33fb85456f41ff0c8f7571af7c7c8371c23a774e
Mo Dongsong <modongsong@126.com>
2018-11-08 18:15:35 +0800

    Updated:

--- components/nexus3/deploy.sh
+++ components/nexus3/deploy.sh
@@ -10,6 +10,6 @@ DOCKER_PROXY="10.146.3.105:8083/"
 sudo docker stop nexus-repos
 sudo docker rm nexus-repos
 
-sudo docker run -d --name=nexus-repos -p 8081:8081 -v ${home}/nexus-data:nexus-data nexus-repos:1.0
+sudo docker run -d --name=nexus-repos -p 8081:8081 -v ${home}/nexus-data:/nexus-data nexus-repos:1.0
 
 cd $curDir
