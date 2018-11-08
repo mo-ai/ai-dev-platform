118ee07b530da92e83fa81ef030fda252cfb7e67
Mo Dongsong <modongsong@126.com>
2018-11-08 15:04:52 +0800

    Updated:

--- components/nexus3/deploy.sh
+++ components/nexus3/deploy.sh
@@ -5,12 +5,11 @@ baseDir=$(dirname $0)
 cd $baseDir
 home=`pwd`
 
+DOCKER_PROXY="10.146.3.105:8083/"
+
 sudo docker stop nexus-repos
 sudo docker rm nexus-repos
 
-sudo docker run -d -p 8081:8081 \
-      --name nexus-repos \
-      -v $home/nexus-data:/nexus-data \
-      nexus-repos:1.0
+sudo docker run -d --name=nexus-repos -p 8081:8081 -v ${home}/nexus-data:nexus-data nexus-repos:1.0
 
 cd $curDir
