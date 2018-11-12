2817aa132641744388b643a087ef1e23375f3d4b
Mo Dongsong <modongsong@126.com>
2018-11-12 16:06:00 +0800

    Updated:

--- components/rancher/deploy-rancher.sh
+++ components/rancher/deploy-rancher.sh
@@ -13,9 +13,9 @@ $docker_cmd stop rancher
 $docker_cmd rm rancher
 
 $docker_cmd run -d --name rancher --restart=unless-stopped \
-  -e http_proxy="$HTTP_PROXY" \
-  -e https_proxy="$HTTP_PROXY" \
-  -e no_proxy="localhost,127.0.0.1,10.146.0.0/16,10.167.0.0/16" \
+  -e HTTP_PROXY="$HTTP_PROXY" \
+  -e HTTPs_PROXY="$HTTP_PROXY" \
+  -e NO_PROXY="localhost,127.0.0.1,10.146.0.0/16,10.167.0.0/16" \
   -p 8089:80 -p 8443:443 \
   -v $home/rancher:/var/lib/rancher \
   ${DOCKER_PROXY}rancher/rancher:latest
