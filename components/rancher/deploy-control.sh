56da4388b278e48e999e07a3230f7cc25a6d3cc2
Mo Dongsong <modongsong@126.com>
2018-11-15 16:03:26 +0800

    Updated:

--- components/rancher/deploy-control.sh
+++ components/rancher/deploy-control.sh
@@ -4,9 +4,9 @@ source config.default
 
 sudo docker run -d --privileged --restart=unless-stopped \
      --net=host -v /etc/kubernetes:/etc/kubernetes -v /var/run:/var/run \
-     ${DOCKER_PROXY}rancher/rancher-agent:v2.0.8 \
+     ${DOCKER_PROXY}rancher/rancher-agent:v2.1.1 \
      --server https://${SERVER_ADDRESS}:8443 \
-     --token lkgjbn49ww4blqs4zkf9f8k44clngtzh9x5v64n2fs5htt9gb8tqqv \
-     --ca-checksum 518535daa15f4bd507afd766183fefefadd6f93690c74f64942703cacdea3d94 \
+     --token ${TOKEN} \
+     --ca-checksum ${CA_CHECKSUM} \
      --address ${LOCAL_ADDRESS} --internal-address ${LOCAL_ADDRESS} \
      --etcd --controlplane --worker
