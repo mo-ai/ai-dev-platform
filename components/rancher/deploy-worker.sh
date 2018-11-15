dad0e07493c8aca41eb53dd72308810b31d68ff3
Mo Dongsong <modongsong@126.com>
2018-11-15 17:28:16 +0800

    Updated:

--- components/rancher/deploy-worker.sh
+++ components/rancher/deploy-worker.sh
@@ -6,8 +6,8 @@ sudo docker run -d --privileged --restart=unless-stopped \
      --net=host -v /etc/kubernetes:/etc/kubernetes \
      -v /var/run:/var/run ${DOCKER_PROXY}rancher/rancher-agent:v2.1.1 \
      --server https://${SERVER_ADDRESS}:8443 \
-     --token lkgjbn49ww4blqs4zkf9f8k44clngtzh9x5v64n2fs5htt9gb8tqqv \
-     --ca-checksum 518535daa15f4bd507afd766183fefefadd6f93690c74f64942703cacdea3d94 \
+     --token ${TOKEN} \
+     --ca-checksum ${CA_CHECKSUM} \
      --address ${LOCAL_ADDRESS} --internal-address ${LOCAL_ADDRESS} \
      --node-name ${NODE_NAME} \
      --worker
