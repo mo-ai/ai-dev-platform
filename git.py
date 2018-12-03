4a4d4251d021885d63fffb5e7da2d652a10da22d
Mo Dongsong <modongsong@126.com>
2018-12-03 18:52:57 +0800

    Updated:

---
+++ git.py
@@ -0,0 +1,74 @@
+git_server = ""
+
+repositories = \
+    {
+        "ai_abilities": {
+            "ai_audio_recognition": {
+                "ai_audio_recognition_musicgene": "lingxiyun/musicgeneai.git"
+            },
+            "ai_cartoon": {
+                "ai_cartoon_characterdetection": "lingxiyun/characterdetectionai.git",
+                "ai_cartoon_control": "lingxiyun/cartoon_control.git",
+                "ai_cartoon_cos": "lingxiyun/instant_cos.git",
+                "ai_cartoon_coverextract": "lingxiyun/coverextractionai.git",
+                "ai_cartoon_ipcharacter": "lingxiyun/relevantcharacterai.git"
+            },
+            "ai_face": {
+                "ai_face_caffee": "lingxiyun/face_pay_coffee.git",
+                "ai_face_music": "miguai/music_ai.git"
+            },
+            "ai_face2cartoon": {
+                "face2cartoon-docker": "miguai/face2cartoon-docker.git",
+                "face2cartoon-second_phrase": "miguai/face2cartoon-second_phrase.git",
+                "pk-ai": "miguai/pk-ai.git"
+            },
+            "ai_video_action": {
+                "ai_video_action_dance": "lingxiyun/dance.git",
+                "ai_video_action_exercise": "lingxiyun/shanpaoai.git"
+            },
+            "ai_video_clip": {
+                "ai_videoclip_game": "lingxiyun/gameai.git",
+                "ai_videoclip_music": "lingxiyun/Concert_Clip_boshi.git",
+                "ai_videoclip_premierleague": "lingxiyun/premier_league.git",
+                "ai_videoclip_worldcup2018": "lingxiyun/aihighlightclip_worldcup2018.git"
+            },
+            "ai_video_live": {
+                "ai_video_live_emotion": "lingxiyun/mgaiemo.git"
+            },
+            "ai_video_process": {
+                "ai_video_process_optimization": "lingxiyun/video_optimization.git"
+            },
+            "ai_video_recognition": {
+                "ai_video_recognition_ad": "lingxiyun/mgaiadadv.git",
+                "ai_video_recognition_adcar": "lingxiyun/mgaiadcar.git",
+                "ai_video_recognition_adstar": "lingxiyun/mgaiadstar.git",
+                "ai_video_recognition_label": "lingxiyun/video-labelling.git",
+                "ai_video_recognition_search": "lingxiyun/video_search.git"
+            }
+        },
+        "ai_deepwise": {
+            "knowledge-graph": "lingxiyun/knowledge-graph.git",
+            "recommend-system": "lingxiyun/recommend-system.git"
+        },
+        "ai_incubating": {
+            "ai_incubating_videoclip": "miguai/ai_incubating_videoclip.git"
+        },
+        "ai_platform": {
+            "pf_console": "Planet/Saturn.git",
+            "pf_infer": "Planet/Venus.git",
+            "pf_learning": "Planet/Mars.git",
+            "pf_mark": "Planet/Jupiter.git",
+            "pf_model": "Planet/Uranus.git",
+            "pf_pass": "Planet/Mercury.git"
+        }
+    }
+
+
+def update():
+
+    return
+
+
+def clone():
+    return
+
